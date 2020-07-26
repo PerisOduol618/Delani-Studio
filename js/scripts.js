0cc8317f93775c9b71ad923ba8b2607b5c561147
PerisOduol618 <oduolpepe618@gmail.com>
2020-07-26 03:11:32 +0300

    Add classes to the icons

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $(".design-1").click(function () {
+        $(".first").toggle();
+        $(".des").toggle();
+    });
+});
+$(document).ready(function () {
+    $(".design-2").click(function () {
+        $(".second").toggle();
+        $(".dev").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $(".design-3").click(function () {
+        $(".third").toggle();
+        $(".pro").toggle();
+    });
+});
